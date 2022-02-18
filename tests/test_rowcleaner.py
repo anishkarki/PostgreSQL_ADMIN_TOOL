8d42e3ad719a6d1e2084a3eedc6eb4756fd20841
anishkarki <anish.karki3579@gmail.com>
2022-02-18 22:54:54 +0545

    circleci2

--- tests/test_rowcleaner.py
+++ tests/test_rowcleaner.py
@@ -3,13 +3,15 @@ import psycopg2
 import time
 
 def create_dummy_db():
-    time.sleep(120)
-    conn = psycopg2.connect("dbname='test' user='user' host='127.0.0.1' password='admin' port='54320'")
-    cur = conn.cursor()
-    cur.execute("create table public.historical_claim_responses (id integer, data jsonb, response jsonb);")
-    cur.execute("insert into public.historical_claim_responses select i,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) ||'\",\"address\":\"'||md5(random()::text)||'\"}')::jsonb,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) || '\",\"address\":\"' || md5(random()::text)||'\"}')::jsonb from generate_series(1,10000) s(i);")
-    conn.commit()
-    conn.close()
+    try:
+        conn = psycopg2.connect("dbname='postgres' user='user' host='127.0.0.1' password='admin' port='54320'")
+        cur = conn.cursor()
+        cur.execute("create table public.historical_claim_responses (id integer, data jsonb, response jsonb);")
+        cur.execute("insert into public.historical_claim_responses select i,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) ||'\",\"address\":\"'||md5(random()::text)||'\"}')::jsonb,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) || '\",\"address\":\"' || md5(random()::text)||'\"}')::jsonb from generate_series(1,10000) s(i);")
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(e)
 
 def test_queryexecuter():
     a = rowcleaner.connect_execute()
