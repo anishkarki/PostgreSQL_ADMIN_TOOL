d5b8b8285d1a16bad3b9ddf9d3c53daff97f7d25
anishkarki <anish.karki3579@gmail.com>
2022-02-18 23:19:09 +0545

    circleci2

--- tests/test_rowcleaner.py
+++ tests/test_rowcleaner.py
@@ -1,23 +1,25 @@
 from postgresrowscleaner import rowcleaner, queryexecuter
-import psycopg2
 import time
 
-def create_dummy_db():
+
+def test_create_dummy_db():
+    time.sleep(120)
     try:
-        conn = psycopg2.connect("dbname='postgres' user='user' host='127.0.0.1' password='admin' port='54320'")
-        cur = conn.cursor()
-        cur.execute("create table public.historical_claim_responses (id integer, data jsonb, response jsonb);")
-        cur.execute("insert into public.historical_claim_responses select i,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) ||'\",\"address\":\"'||md5(random()::text)||'\"}')::jsonb,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) || '\",\"address\":\"' || md5(random()::text)||'\"}')::jsonb from generate_series(1,10000) s(i);")
-        conn.commit()
+        conn,cur = queryexecuter.connect()
+        cur.execute("create table public.historical_claim_responses (id integer, data jsonb, response jsonb)")
+        print("success")
+        cur.execute("insert into postgres.public.historical_claim_responses select i,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) ||'\",\"address\":\"'||md5(random()::text)||'\"}')::jsonb,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) || '\",\"address\":\"' || md5(random()::text)||'\"}')::jsonb from generate_series(1,10000) s(i);")
         conn.close()
     except Exception as e:
         print(e)
 
+
 def test_queryexecuter():
     a = rowcleaner.connect_execute()
+    print(a)
     assert "PostgreSQL" in a.fetchall()[0][0]
 
 def test_row_cleaner():
-    b = rowcleaner.row_cleaner(tablename="historical_claim_reponses")
+    b = rowcleaner.row_cleaner(tablename="historical_claim_responses")
     assert "VACUUM" == b.statusmessage
 
