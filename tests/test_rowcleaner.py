f33bbf9b0f7a9329b2f9ff02a4980edb6f43556d
anishkarki <anish.karki3579@gmail.com>
2022-02-18 23:25:27 +0545

    circleci2

--- tests/test_rowcleaner.py
+++ tests/test_rowcleaner.py
@@ -8,7 +8,7 @@ def test_create_dummy_db():
         conn,cur = queryexecuter.connect()
         cur.execute("create table public.historical_claim_responses (id integer, data jsonb, response jsonb)")
         print("success")
-        cur.execute("insert into postgres.public.historical_claim_responses select i,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) ||'\",\"address\":\"'||md5(random()::text)||'\"}')::jsonb,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) || '\",\"address\":\"' || md5(random()::text)||'\"}')::jsonb from generate_series(1,10000) s(i);")
+        cur.execute("insert into public.historical_claim_responses select i,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) ||'\",\"address\":\"'||md5(random()::text)||'\"}')::jsonb,('{\"firstName\":\"' ||  md5(random()::text) || '\", \"lastName\":\"' || md5(random()::text) || '\",\"address\":\"' || md5(random()::text)||'\"}')::jsonb from generate_series(1,10000) s(i);")
         conn.close()
     except Exception as e:
         print(e)
